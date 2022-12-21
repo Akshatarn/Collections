4aeede8be581d8cfe398291df54458731285acef
akshata_rn <akshatarn123@gamail.com>
2022-12-21 08:44:05 +0530

    Collection for each data structures like stack,queuelinkedlist

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                CollectionImplementation.TestList();
+                CollectionImplementation.TestSortedList();
+                CollectionImplementation.DoListDemo();
+                CollectionImplementation.DoStackDemo();
+                CollectionImplementation.DoQueueDemo();
+                CollectionImplementation.DoSetDemo();
+                CollectionImplementation.DoDictionaryDemo();
+            }
+        }
+    }
+}
